4558de9b618234a9e1f7bef343239f0d583d69ef
Hugo Miranda Campos <hugomirandacampos@gmail.com>
2022-04-12 20:48:44 -0300

    commit 2: se agregan algunas funcionalidades al proyecto

--- js/script.js
+++ js/script.js
@@ -1,23 +1,55 @@
+let nombre;
+let nombreReceta;
 let ingrediente;
 let ingredientes = [];
+let id = 0;
+let idReceta;
+let continuar = "s";
 let continuarCargaDeIngredientes = "s";
 let ingredienteCorrecto = "s";
+let recetas = [];
+
+class Receta
+{
+    constructor(id, nombre, ingredientes)
+    {
+        this.id = id;
+        this.nombre = nombre;
+        this.ingredientes = ingredientes;
+    }
+}
+
+for(let i=0; i<recetas.length; i++)
+{
+    console.log(recetas[i] + "\n");
+}
 
 do
 {
-    ingrediente = prompt("Escriba el nuevo ingrediente a agregar a la lista: ");
+    id++;
+    idReceta = id;
 
-    ingredientes.push(ingrediente);
+    nombre = prompt("Escriba el nombre de la receta: ");
+    
+    do
+    {
+        ingrediente = prompt("Escriba el ingrediente para hacer la receta: ");
+    
+        ingredientes.push(ingrediente);
+    
+        continuarCargaDeIngredientes = prompt("¿Desea seguir agregando ingredientes? (s/n)");
+    
+    }while(continuarCargaDeIngredientes !== "n");
 
-    continuarCargaDeIngredientes = prompt("¿Desea seguir agregando ingredientes? (s/n)");
+    nombreReceta = nombre;
 
-}
-while(continuarCargaDeIngredientes !== "n");
+    nombreReceta = new Receta(idReceta, nombre, ingredientes);
 
-for(let i=0; i<ingredientes.length; i++)
-{
-    console.log(ingredientes[i] + "\n");
-}
+    recetas.push(nombreReceta);
+
+    continuar = prompt("¿Desea seguir agregando recetas? (s/n)");
+
+}while(continuar !== "n");
 
 
 /*ingredienteCorrecto = prompt("El ingrediente es " + ingrediente + ": ¿Estás seguro? (s/n)");
