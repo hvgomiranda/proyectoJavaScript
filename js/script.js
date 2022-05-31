e68ee7ae756a3d0aa6dbf5bf1344ed27c2838cb8
Hugo Miranda Campos <hugomirandacampos@gmail.com>
2022-05-31 20:49:17 -0300

    commit 9c: se arreglan algunos errores menores

--- js/script.js
+++ js/script.js
@@ -1,5 +1,57 @@
 let ingredientes = [];
 let lista;
+let flag = 1;
+
+function search()
+{
+    button = document.getElementById("botonAgregar");
+    let listado = document.querySelector("#listado");
+    if(flag)
+    {
+        button.innerText = "Volver";
+        flag = 0;
+        //listado.style.display = "none";
+
+        fetch("../baseDeDatos/baseDeDatosRecetas.json")
+        .then(resp => resp.json())
+        .then(data => 
+            {
+                data.forEach(receta =>
+                {
+                    for(let i=0; i<ingredientes.length; i++)
+                    {
+                        for(let j=0; j<receta.lenght; j++)
+                        {
+                            for(let k=0; k<receta.ingrediente.lenght; k++)
+                            {
+                                if(ingredientes[i] == receta[j].ingrediente[k])
+                                {
+                                    html = "";
+                                        html+=`
+                                        <li>
+                                            <p>${receta.nombre}</p>
+                                        </li>`
+                                }
+
+                            }
+                        }
+                    }
+            });
+            lista.innerHTML = html;
+        });
+
+
+    }
+    else
+    {
+        button.innerText = "Buscar";
+        flag = 1;
+        listado.style.display = "";
+
+    }
+
+    
+}
 
 //aplicar color al item seleccionado y agregarlo a una lista
 document.querySelector("ul").addEventListener("click", function(e) {
@@ -36,3 +88,4 @@ fetch("../baseDeDatos/baseDeDatos.json")
             });
             lista.innerHTML = html;
         });
+        
